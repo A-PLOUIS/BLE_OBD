816f63b8cde829a0ba8ea53512a56e56d492c6f4
A-PLOUIS <antonio.pierre.louis@gmail.com>
2015-09-08 14:38:26 -0400

    Added a progress dialog during connexion to OBD interface
    Inversed writing and reading uuid.
    Only command "AT Z" is sent.
    Added logs

--- app/src/main/java/com/chabintech/ble_obd/MainActivity.java
+++ app/src/main/java/com/chabintech/ble_obd/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chabintech.ble_obd;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -11,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.UiThread;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 
 import com.chabintech.ble_obd.dialog.BLEDevicesDialog;
 import com.chabintech.ble_obd.fragments.MainFragment;
+import com.chabintech.ble_obd.utils.LogUtils;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -33,8 +36,11 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
 
     //Bluetooth UUID
     private static final UUID mServiceUUID = UUID.fromString("0000FFF0-0000-1000-8000-00805F9B34FB");
-    private static final UUID mWriteUUID = UUID.fromString("0000FFF1-0000-1000-8000-00805F9B34FB");
-    private static final UUID mReadUUID = UUID.fromString("0000FFF2-0000-1000-8000-00805F9B34FB");
+    private static final UUID mWriteUUID = UUID.fromString("0000FFF2-0000-1000-8000-00805F9B34FB");
+    private static final UUID mReadUUID = UUID.fromString("0000FFF1-0000-1000-8000-00805F9B34FB");
+
+    private DialogFragment mDevicesDialog;
+    private ProgressDialog mWaitingDialog;
 
     private String mCurrentCommand;
 
@@ -47,18 +53,28 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
     private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
         @Override
         public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-            super.onCharacteristicRead(gatt, characteristic, status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(LOG_GATT, "Characteristic " + characteristic.getUuid().toString() + " value : " + Arrays.toString(characteristic.getValue()));
+                LogUtils.logInfo("Read characteristic value " + Arrays.toString(characteristic.getValue()) + " in FFF1");
+            } else {
+                LogUtils.logInfo("Couldnt read characteristic " + mReadUUID.toString());
+            }
 
-            Log.d(LOG_GATT, "Characteristic " + characteristic.getUuid().toString() + " value : " + Arrays.toString(characteristic.getValue()));
             mWaitingForResult = false;
         }
 
         @Override
         public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             if (status == BluetoothGatt.GATT_SUCCESS) {
+                LogUtils.logInfo("Command wroten");
                 Log.d(LOG_GATT, "Characteristic " + characteristic.getUuid().toString() + " write : " + Arrays.toString(characteristic.getValue()));
                 readOBDResult();
             }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            LogUtils.logInfo(characteristic.getUuid().toString() + " value have been updated :\nValue = " + Arrays.toString(characteristic.getValue()));
         }
 
         @Override
@@ -66,9 +82,9 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 mIsConnected = true;
                 Log.i(LOG_GATT, "******Connected to GATT server*****");
-                Log.i(LOG_GATT, "Attempting to start service discovery:" +
-                        mBluetoothGatt.discoverServices());
-
+                mWaitingDialog.dismiss();
+                Log.i(LOG_GATT, "Attempting to start service discovery");
+                mBluetoothGatt.discoverServices();
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 mIsConnected = false;
                 Log.i(LOG_GATT, "******Disconnected from GATT server******");
@@ -81,6 +97,7 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
                 Log.i(LOG_GATT, "******Services discovered*****");
                 for (BluetoothGattService service : gatt.getServices()) {
                     Log.i(LOG_GATT, "UUID : " + service.getUuid().toString());
+                    LogUtils.logInfo(service.getUuid().toString());
                 }
 
                 initialiseOBDInterface();
@@ -110,6 +127,10 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
         }
         devices = new HashSet<>();
         mHandler = new Handler();
+        mWaitingDialog = new ProgressDialog(this);
+        mWaitingDialog.setMessage("Scanning");
+        mWaitingDialog.setIndeterminate(true);
+        mWaitingDialog.setCancelable(false);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
@@ -146,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
                     mScanning = false;
                     Log.d(LOG_SCAN, "******Stopping BLEScan*****");
                     mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    mWaitingDialog.dismiss();
                     showDevicesDialog();
                 }
             }, SCAN_PERIOD);
@@ -153,21 +175,27 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
             mScanning = true;
             Log.d(LOG_SCAN, "*****Starting BLEScan*****");
             mBluetoothAdapter.startLeScan(mLeScanCallback);
+            mWaitingDialog.show();
         } else {
             mScanning = false;
             Log.d(LOG_SCAN, "******Stopping BLEScan*****");
             mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            mWaitingDialog.dismiss();
         }
     }
 
     @UiThread
     private void showDevicesDialog() {
         Log.d(LOG_SCAN, "Showing devices dialog");
-        BLEDevicesDialog.newInstance(devices).show(getSupportFragmentManager(), "dialogdevices");
+        mDevicesDialog = BLEDevicesDialog.newInstance(devices);
+        mDevicesDialog.show(getSupportFragmentManager(), "dialogdevices");
     }
 
     @Override
     public void connectToBLEDevice(BluetoothDevice p_device) {
+        mDevicesDialog.dismiss();
+        mWaitingDialog.setMessage("Connecting to Device");
+        mWaitingDialog.show();
         mBluetoothGatt = p_device.connectGatt(this, false, mGattCallback);
     }
 
@@ -181,30 +209,32 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
 
     private void initialiseOBDInterface() {
         writeOBDCommand("AT Z");
+        /*
         writeOBDCommand("AT E0");
         writeOBDCommand("AT ST " + Integer.toHexString(0xFF & 62));
-        writeOBDCommand("AT SP 0");
+        writeOBDCommand("AT SP 0");*/
     }
 
     private void writeOBDCommand(String p_command) {
         if (mIsConnected) {
-            while (mWaitingForResult) {
-                try {
-                    this.wait(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+//            while (mWaitingForResult) {
+//                try {
+//                    this.wait(50);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
             BluetoothGattCharacteristic writeChar = mBluetoothGatt.getService(mServiceUUID)
                     .getCharacteristic(mWriteUUID);
-            byte[] data = (p_command + '\r').getBytes();
 
-            writeChar.setValue(data);
+            LogUtils.logInfo("About to write " + p_command + " in FFF1");
+
+            writeChar.setValue(p_command + '\r');
             mBluetoothGatt.writeCharacteristic(writeChar);
             mWaitingForResult = true;
             mCurrentCommand = p_command;
         } else {
-            Toast.makeText(getBaseContext(), "Can't write OBD Command\nNot Connected", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Can't write OBD Command\nNot Connected", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -212,9 +242,11 @@ public class MainActivity extends AppCompatActivity implements BLEDevicesDialog.
         if (mIsConnected) {
             BluetoothGattCharacteristic readChar = mBluetoothGatt.getService(mServiceUUID)
                     .getCharacteristic(mReadUUID);
+            LogUtils.logInfo("About to read result");
             mBluetoothGatt.readCharacteristic(readChar);
         } else {
-            Toast.makeText(getBaseContext(), "Can't read OBD Result\nNot Connected", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Can't read OBD Result\nNot Connected", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
